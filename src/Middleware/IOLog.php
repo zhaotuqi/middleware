e8f4d07c0627ec0a3ca06027dc094a3ea36a6314
shaobaojie <baojie.shao@wenba100.com>
2017-02-19 17:44:18 +0800

    middle

---
+++ src/Middleware/IOLog.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Log;
+use Common;
+use Monolog\Logger;
+
+class IOLog
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        $message = [
+            'request_uri'    => $request->getPathInfo(),
+            'request_header' => $request->headers->get('Content-Type'),
+            'request_body'   => $request->all(),
+            'response_body'  => @json_decode($response->getContent(), true) ?: $response->getContent()
+        ];
+
+        Common::logger(config('app.app_name'),
+            'requestlog.log',
+            json_encode($message, JSON_UNESCAPED_UNICODE),
+            Logger::INFO
+        );
+
+    }
+
+
+}
