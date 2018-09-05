d417cfd451949cc07b7a9b5c3737f40fafd31033
程广岩 <guangyan.cheng@wenba100.com>
2018-09-05 13:01:40 +0800

    Update IOLog.php

--- src/Http/Middleware/IOLog.php
+++ src/Http/Middleware/IOLog.php
@@ -58,7 +58,7 @@ class IOLog
 
 	    // 生产环境上报接口状态
 	    if (app()->environment() == 'production' || app()->environment() == 'pro') {
-		    if (env("FALCON") && class_exists('Monitor')) {
+		    if (env("FALCON") && class_exists('Monitor\Client')) {
 		        try {
                     Monitor\Client::inc($request->getPathInfo() . "_qpm");
                     Monitor\Client::cost($request->getPathInfo() . "_cost", $cost_time * 1000); // 耗时是以毫秒计算
